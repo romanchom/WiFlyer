9c36de1089308d602d5ba8e837d896d24844fe5d
Roman Chomik <romanchom@gmail.com>
2019-04-21 14:32:44 +0200

    WIP motor control and stabilization. Added missing files.

---
+++ Messages.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+struct PIDMessage
+{
+    enum { id = 0 };
+    float rollP;
+    float rollI;
+    float rollD;
+    float pitchP;
+    float pitchI;
+    float pitchD;
+};
+
+struct SteeringMessage
+{
+    enum { id = 1 };
+    int enabled;
+    float throttle;
+};
